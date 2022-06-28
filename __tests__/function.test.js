43953cbf8ab2cc4d7d2cc487b066070c9ea04c6a
akshay.rein-at-908805445373 <waizaeer4@gmail.com>
2022-06-28 11:56:17 +0530

    as

--- __tests__/function.test.js
+++ __tests__/function.test.js
@@ -1,5 +1,17 @@
 import {sum, reverseNumber, reverseAsync} from '../src/sum';
 
+beforAll(() => {
+  // ! before everything
+});
+
+afterAll(() => {
+  // ! after everything
+});
+
+beforeEach(() => {
+  // * maybe update DB, orr setup Redux
+});
+
 test('Sum test', () => {
   expect(sum(2, 2)).toBe(4);
 });
@@ -11,10 +23,22 @@ test('Revers number', () => {
   expect(reverseNumber(-74584)).toBe(-48547);
 });
 
-test('Revers number', () => {
+test('Revers number async', () => {
   expect(reverseAsync(12345)).resolves.toBe(54321);
 
   //   expect(await reverseAsync(-123)).toBe(-321);
   //   expect(await reverseAsync(75412)).toBe(21457);
   //   expect(await reverseAsync(-74584)).toBe(-48547);
+});
+
+test('Revers number async', () => {
+  expect(reverseAsync(12345)).resolves.toBe(54321);
+
+  //   expect(await reverseAsync(-123)).toBe(-321);
+  //   expect(await reverseAsync(75412)).toBe(21457);
+  //   expect(await reverseAsync(-74584)).toBe(-48547);
+});
+
+afterEach(() => {
+  // * Cleanup
 });
